4a5c25afa32b56c58a0e33d0807a8b8aa80c74b3
TheDivineAnt <elimat79@kingshigh.school.nz>
2026-05-12 11:01:21 +1200

    flashlight done
    it's looking good

--- final-game/Scripts/character_body_2d.gd
+++ final-game/Scripts/character_body_2d.gd
@@ -1,5 +1,16 @@
 extends CharacterBody2D
 
+func _process(_delta):
+	var target_angle = (get_global_mouse_position() - global_position).angle()
+	rotation = lerp_angle(rotation, target_angle, 10)
+	# This makes the player node rotate to face the mouse cursor
+	look_at(get_global_mouse_position())
+func _input(event):
+	# Check for a right-click to toggle the light
+	if event.is_action_pressed("ui_right"):
+		$PointLight2D.enabled = !$PointLight2D.enabled
+
+
 ## The maxiumum speed allowed, in pixels per second
 @export var max_speed:  float = 200.0
 ## Accleration as form of [code]pixels/s2 [/code]. [br]
@@ -15,9 +26,3 @@ func _adjusted_acceleration(delta:float) -> float:
 
 func _direction() -> Vector2:
 	return Input.get_vector("left", "right" , "up" , "down")
-
-
-@onready var flash_light: Pointlight2D = $flashlight
-
-if targetPosition:
-	flash.look_at(targetPosition)
